42a31908b7570ff497d8cc051cbe6fd869ec4220
Canwdev <c2097@live.com>
2024-09-01 17:37:04 +0800

    update

--- debian-kasmvnc/init.sh
+++ debian-kasmvnc/init.sh
@@ -1,7 +1,8 @@
 docker rm -f dkvnc
 
+#  -v $PWD/config-override/.vnc/kasmvnc.yaml:/root/.vnc/kasmvnc.yaml \
+#  -v $PWD/config-override/ssl:/root/ssl \
+
 docker run --restart=always --name dkvnc \
   --net=host \
-  -v $PWD/config-override/.vnc/kasmvnc.yaml:/root/.vnc/kasmvnc.yaml \
-  -v $PWD/config-override/ssl:/root/ssl \
   -d debian-kasm-vnc
