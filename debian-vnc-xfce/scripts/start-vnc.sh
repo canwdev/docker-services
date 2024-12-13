821302d974391947301319be41ce97ea5812f8b7
canwdev <c2097@live.com>
2024-12-13 02:24:39 -0800

    password

--- debian-vnc-xfce/scripts/start-vnc.sh
+++ debian-vnc-xfce/scripts/start-vnc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+export USER=${USER:-root}
+
+# 检查是否设置了 VNC 密码环境变量
+if [ -z "$VNC_PASSWORD" ]; then
+    echo "Warning: VNC_PASSWORD is not set. Using default password."
+    VNC_PASSWORD="password"
+fi
+
+echo "$VNC_PASSWORD" | vncpasswd -f > /root/.vnc/passwd
+
 # 启动 VNC 服务器并设置显示参数
 vncserver :1 -geometry 1366x768 -depth 24
 
